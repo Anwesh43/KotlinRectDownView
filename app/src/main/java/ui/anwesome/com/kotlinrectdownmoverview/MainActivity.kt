098a96ff243f7119e85fcd5c768100783fcff8a7
Anwesh43 <anweshthecool0@gmail.com>
2018-03-02 23:19:34 +0530

    adding RectDownView in Activity

--- app/src/main/java/ui/anwesome/com/kotlinrectdownmoverview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinrectdownmoverview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinrectdownmoverview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.rectdownmoverview.RectDownMoverView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RectDownMoverView.create(this, "Hi all people I am back")
     }
 }
